f0b4e3d657d425aa58bb36b15121dc4f86fa6c28
JamirLeal <42588046+JamirLeal@users.noreply.github.com>
2019-11-13 19:00:20 -0600

    Create FA.vhd

---
+++ practica5/FA.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:39:43 10/09/2019 
+-- Design Name: 
+-- Module Name:    FullAdder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FullAdder is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Cin : in  STD_LOGIC;
+           S : out  STD_LOGIC;
+           Cout : out  STD_LOGIC);
+end FullAdder;
+
+architecture Behavioral of FullAdder is
+
+begin
+
+	S <= (A xor B) xor Cin;
+	Cout <= (A and B) or ((A xor B) and Cin);
+
+end Behavioral;
